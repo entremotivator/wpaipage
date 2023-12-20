c62e3587d018b1d456ec0ad3f6c84530b5540642
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-20 16:00:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import requests
 
+# Placeholder functions for Divi page creation and WordPress interaction
+def create_divi_page(api_key, page_title, page_content, uploaded_file):
+    try:
+        # Implement logic to create a new Divi page
+        # You can use the uploaded_file parameter to handle file uploads
+        return "123"  # Placeholder ID, replace with actual logic
+    except Exception as e:
+        st.error(f"Error creating Divi page: {e}")
+        return None
+
+def set_home_page(api_key, page_id):
+    try:
+        # Implement logic to set the newly created page as the homepage
+        pass
+    except Exception as e:
+        st.error(f"Error setting the homepage: {e}")
+
+def upload_to_ftp(ftp_credentials, uploaded_file):
+    try:
+        # Implement logic to upload files to WordPress through FTP
+        # Use ftp_credentials dictionary for FTP connection details
+        pass
+    except Exception as e:
+        st.error(f"Error uploading files to FTP: {e}")
+
 def register_user(api_url, api_key, customer_info, site_url, site_title, membership_status, payment_status):
     endpoint = f"{api_url}/wp-json/wu/v2/register"
     headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
@@ -19,68 +44,187 @@ def register_user(api_url, api_key, customer_info, site_url, site_title, members
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
 
-def display_registration_form():
-    st.title("WP AI Registration App")
+# Apply some basic styling using HTML and CSS
+st.markdown("""
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+        }
+        .sidebar .sidebar-content {
+            background-color: #f5f5f5;
+            padding: 1rem;
+            border-radius: 10px;
+        }
+        .main {
+            padding: 2rem;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-    # Sidebar for API configuration
-    with st.sidebar:
-        st.subheader("API Configuration")
-        api_url = st.text_input("API URL:")
-        api_key = st.text_input("API Key:", type="password")
-
-    st.write("## User Information")
-    username = st.text_input("Username:")
-    password = st.text_input("Password:", type="password")
-    
-    # Password strength checker
-    if len(password) < 8:
-        st.warning("Password should be at least 8 characters long.")
-    
-    email = st.text_input("Email:")
-    
-    # Email validation
-    if not st.button("Validate Email"):
-        st.warning("Click the button to validate email.")
-
-    st.write("## Site Information")
-    site_url = st.text_input("Site URL:")
-    site_title = st.text_input("Site Title:")
-
-    st.write("## Membership and Payment Options")
-    membership_status = st.selectbox("Membership Status", ["pending", "active", "trialing", "expired", "on-hold", "canceled"])
-    payment_status = st.selectbox("Payment Status", ["pending", "completed", "refunded", "partially-refunded", "partially-paid", "failed", "canceled"])
-
-    return api_url, api_key, {"username": username, "password": password, "email": email}, site_url, site_title, membership_status, payment_status
-
+# Streamlit app
 def main():
-    form_values = display_registration_form()
+    st.title("AI Funnel Machine")
+
+    # Sidebar for FTP credentials and OpenAI key
+    with st.sidebar:
+        st.subheader("FTP Credentials")
+        ftp_host = st.text_input("FTP Host")
+        ftp_user = st.text_input("FTP User")
+        ftp_password = st.text_input("FTP Password", type="password")
+
+        st.subheader("OpenAI Key")
+        openai_key = st.text_input("Enter your OpenAI key")
+
+    # Main content for Divi page creation
+    with st.container():
+        st.header("Divi Page Creation")
+
+        # Get the number of page sections
+        num_sections = st.number_input("Number of Page Sections", min_value=1, value=1)
+
+        # Loop through each section
+        for i in range(num_sections):
+            st.subheader(f"Section {i + 1}")
+
+            # Get page details for each section
+            page_title = st.text_input(f"Enter the title for Section {i + 1}")
+            page_content = st.text_area(f"Enter the content for Section {i + 1}", height=200)
+
+            # File upload for each section
+            uploaded_file = st.file_uploader(f"Upload file for Section {i + 1}", type=["jpg", "jpeg", "png", "pdf"])
+
+            # Create Divi page for each section
+            if st.button(f"Create Divi Page for Section {i + 1}"):
+                with st.spinner(f"Creating Divi Page for Section {i + 1}..."):
+                    page_id = create_divi_page(openai_key, page_title, page_content, uploaded_file)
+
+                if page_id:
+                    st.success(f"Divi page created successfully for Section {i + 1} with ID: {page_id}")
+                    # Set the new page as the homepage
+                    set_home_page(openai_key, page_id)
+                    # Upload files to WordPress through FTP
+                    ftp_credentials = {'host': ftp_host, 'user': ftp_user, 'password': ftp_password}
+                    upload_to_ftp(ftp_credentials, uploaded_file)
+
+    # User Registration Section
+    st.header("User Registration")
+
+    # Get user registration form values
+    api_url, api_key, customer_info, site_url, site_title, membership_status, payment_status = display_registration_form()
 
     if st.button("Register User"):
         # Input validation
-        if any(value == '' for value in form_values[:-2]):
-            st.warning("Please fill out all fields.")
-            return
-
-        with st.spinner("Registering user..."):
-            response = register_user(*form_values)
-
-        if "error" in response:
-            st.error(f"Error: {response['error']}")
+        if any(value == '' for value in [api_url, api_key] + list(customer_info.values()) + [site_url, site_title]):
+            st.warning("Please fill out all fields for user registration.")
         else:
-            st.success("User registered successfully!")
-            st.write("## Registration Details")
-            st.write(f"**Username:** {response['customer']['username']}")
-            st.write(f"**Email:** {response['customer']['email']}")
-            st.write(f"**Site Title:** {response['site']['site_title']}")
-            st.write(f"**Membership Status:** {response['membership']['status']}")
-            st.write(f"**Payment Status:** {response['payment']['status']}")
-            # Customize success message based on status
-            if response['membership']['status'] == 'active' and response['payment']['status'] == 'completed':
-                st.success("Congratulations! Your membership is now active, and the payment is completed.")
-            elif response['membership']['status'] == 'pending':
-                st.info("Your membership is pending approval.")
+            with st.spinner("Registering user..."):
+                response = register_user(api_url, api_key, customer_info, site_url, site_title, membership_status, payment_status)
+
+            if "error" in response:
+                st.error(f"Error: {response['error']}")
             else:
-                st.info("Please check your registration details.")
+                st.success("User registered successfully!")
+                st.write("## Registration Details")
+                st.write(f"**Username:** {response['customer']['username']}")
+                st.write(f"**Email:** {response['customer']['email']}")
+                st.write(f"**Site Title:** {response['site']['site_title']}")
+                st.write(f"**Membership Status:** {response['membership']['status']}")
+                st.write(f"**Payment Status:** {response['payment']['status']}")
+                # Customize success message based on status
+                if response['membership']['status'] == 'active' and response['payment']['status'] == 'completed':
+                    st.success("Congratulations! Your membership is now active, and the payment is completed.")
+                elif response['membership']['status'] == 'pending':
+                    st.info("Your membership is pending approval.")
+                else:
+                    st.info("Please check your registration details.")
+
+    # Additional Features and Improvements
+    st.header("Additional Features and Improvements")
+
+    # Dynamic Content for Divi Page Creation
+    st.write("## Dynamic Content for Divi Page Creation")
+
+    # Allow users to dynamically add or remove sections for Divi page creation
+    dynamic_sections = st.checkbox("Enable Dynamic Sections", False)
+    if dynamic_sections:
+        num_dynamic_sections = st.number_input("Number of Dynamic Sections", min_value=1, value=1)
+        for i in range(num_dynamic_sections):
+            st.subheader(f"Dynamic Section {i + 1}")
+            # Additional dynamic content logic goes here
+
+    # Improved File Upload Handling
+    st.write("## Improved File Upload Handling")
+
+    # Provide feedback on file upload progress
+    st.progress(0.5)  # Placeholder for file upload progress
+
+    # Allow users to upload multiple files
+    uploaded_files = st.file_uploader("Upload Multiple Files", type=["jpg", "jpeg", "png", "pdf"], accept_multiple_files=True)
+    if uploaded_files:
+        st.success(f"Uploaded {len(uploaded_files)} files.")
+
+    # Enhanced User Registration Form
+    st.write("## Enhanced User Registration Form")
+
+    # Add more fields to the registration form
+    additional_field = st.text_input("Additional Field")
+
+    # Include additional options for membership and payment status
+    extended_membership_status = st.selectbox("Extended Membership Status", ["custom", "exclusive"])
+
+    # Interactive UI Elements
+    st.write("## Interactive UI Elements")
+
+    # Implement interactive UI elements like sliders, date pickers, and color pickers
+    slider_value = st.slider("Slider", 0, 100, 50)
+    selected_date = st.date_input("Select Date", value=None)
+    selected_color = st.color_picker("Pick a Color", "#00f")
+
+    # User Feedback and Notifications
+    st.write("## User Feedback and Notifications")
+
+    # Provide informative messages and notifications throughout the app
+    st.info("This is an informational message.")
+    st.success("This is a success message.")
+    st.warning("This is a warning message.")
+    st.error("This is an error message.")
+
+    # User Authentication
+    st.write("## User Authentication")
+
+    # Integrate user authentication to secure certain functionalities
+    authenticated = st.checkbox("Authenticated", False)
+    if authenticated:
+        st.success("User is authenticated.")
+    else:
+        st.warning("User is not authenticated.")
+
+    # Documentation and Help Section
+    st.write("## Documentation and Help Section")
+
+    # Include a documentation or help section within the app for user guidance
+    st.markdown("### Welcome to the AI Funnel Machine Documentation")
+    st.markdown("This app provides various features to streamline your AI funnel creation process.")
+    st.markdown("#### Divi Page Creation:")
+    st.markdown("1. Enter the number of page sections.")
+    st.markdown("2. For each section, provide a title, content, and upload a file.")
+    st.markdown("3. Click 'Create Divi Page' to generate pages.")
+
+    # Logging and Error Handling
+    st.write("## Logging and Error Handling")
+
+    # Implement logging functionality
+    log_text = st.text_area("Log:", height=200, value="")
+
+    # Error handling example
+    if st.button("Simulate Error"):
+        try:
+            # Simulate an error for demonstration purposes
+            raise Exception("This is a simulated error.")
+        except Exception as e:
+            st.error(f"Error: {e}")
+            # Log the error
+            log_text += f"\nError: {e}"
 
 if __name__ == "__main__":
     main()
