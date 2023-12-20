130e727685dd0c7f2f10ffd0128fd8f9e3381888
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-20 15:06:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import requests
 import streamlit as st
+import requests
 
 def register_user(api_url, api_key, customer_id, customer_info, membership_status, products, payment_status, site_url, site_title):
     endpoint = f"{api_url}/wp-json/wu/v2/register"
@@ -14,16 +14,20 @@ def register_user(api_url, api_key, customer_id, customer_info, membership_statu
         "site": {"site_url": site_url, "site_title": site_title}
     }
 
-    response = requests.post(endpoint, json=data, headers=headers)
-    return response.json()
+    try:
+        response = requests.post(endpoint, json=data, headers=headers)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return {"error": str(e)}
 
 def main():
     st.title("WP Ultimo Registration App")
 
     api_url = st.text_input("API URL:")
-    api_key = st.text_input("API Key:")
+    api_key = st.text_input("API Key:", type="password")
     customer_id = st.number_input("Customer ID:")
-    
+
     user_id = st.number_input("User ID:")
     username = st.text_input("Username:")
     password = st.text_input("Password:", type="password")
@@ -32,7 +36,7 @@ def main():
     customer_info = {"user_id": user_id, "username": username, "password": password, "email": email}
 
     membership_status = st.selectbox("Membership Status", ["pending", "active", "trialing", "expired", "on-hold", "canceled"])
-    
+
     products = st.text_area("Products (comma-separated):").split(",")
     products = [int(product.strip()) for product in products if product.strip()]
 
@@ -42,8 +46,14 @@ def main():
     site_title = st.text_input("Site Title:")
 
     if st.button("Register User"):
-        response = register_user(api_url, api_key, customer_id, customer_info, membership_status, products, payment_status, site_url, site_title)
-        st.success(f"User registered successfully!\nMembership Information:\n{response}")
+        with st.spinner("Registering user..."):
+            response = register_user(api_url, api_key, customer_id, customer_info, membership_status, products, payment_status, site_url, site_title)
+        
+        if "error" in response:
+            st.error(f"Error: {response['error']}")
+        else:
+            st.success("User registered successfully!")
+            # Display more specific information from the API response, if needed
 
 if __name__ == "__main__":
     main()
